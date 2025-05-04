7c51568d4bb9a94556ddd94ab92175f0d512bac2
mohammad mehdi rashidi <mehdirashidi530@gmail.com>
2025-05-04 15:46:17 +0330

    fixed simple issues

--- calculater.py
+++ calculater.py
@@ -1,44 +1,34 @@
+def calc (first,second,operator):
+
+    if(operator == "*"):
+        return first * second
+    elif(operator == "+"):
+        return first + second
+    elif(operator == "-"):
+        return first - second
+    elif(operator == "/"):
+        try:
+            return first/second 
+        except ZeroDivisionError:
+            print("second should be not zero")
+            return "try again"
+    else:
+        print("invalid operator")
+        return
 while(True):
     print("enter ur first num:")
     try:
-        first = float(input())
+        num1 = float(input())
     except:
         print("enter number not chars")
         continue
     print("enter ur second num:")
     try:
-        second = float(input())
+        num2 = float(input())
     except:
         print("enter number not chars")
         continue
     print("enter ur operator:")
     operator = input()
-    if(isinstance(first , float or int) == False or isinstance(second , float or int) == False):
-        print("wrong inputs try again")
-        continue
 
-    def calc (first,second,operator):
-    
-        if operator in [("+") , ("-") , ("/") , ("*")]:
-            if(operator == "*"):
-                return first * second
-            elif(operator == "+"):
-                return first + second
-            elif(operator == "-"):
-                return first - second
-            elif(operator == "/"):
-                try:
-                    first/second == ZeroDivisionError
-                except:
-                    #raise ZeroDivisionError("secound should be not zero")
-                    print("secound should be not zero")
-                    return "try again"
-                else:
-                    return first / second
-            else:
-                print("wrong nums")
-                return
-        else: 
-            print("wrong operators") 
-            return                 
-    print(calc(first , second , operator))
+    print(calc(num1 , num2 , operator))
